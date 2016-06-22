a0f6831da6fb0fda0a0cf76c272444c798c61039
Tim Mensch <tim-git-tecra@bitgems.com>
2016-06-22 15:44:38 -0600

    Change Node packaging to not use UML wrapper and to save the export as a named member of module.exports so that it can work with TypeScript+Rollup

---
+++ index.js
@@ -0,0 +1,11 @@
+
+computeLayout = require("./src/Layout.js");
+
+module.exports.cssLayout = function (node) {
+    /*eslint-disable */
+    // disabling ESLint because this code relies on the above include
+    computeLayout.fillNodes(node);
+    computeLayout.computeLayout(node);
+    /*eslint-enable */
+};
+
